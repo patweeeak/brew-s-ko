37b457e38617d387553040d7c548e3b841a3cbb2
Patrick <sangranopatrick@gmail.com>
2024-09-03 22:50:59 +0800

    childform added

--- Inventory.cs
+++ Inventory.cs
@@ -12,9 +12,27 @@ namespace brew_s_ko
 {
     public partial class Inventory : Form
     {
+        private Form currentchildForm;
         public Inventory()
         {
             InitializeComponent();
+        }
+        public void OpenChildForm(Form childForm)
+        {
+            if (currentchildForm != null)
+            {
+                currentchildForm.Hide();
+            }
+            currentchildForm = childForm;
+            childForm.TopLevel = false;
+            childForm.Dock = DockStyle.Fill;
+            this.(panelName).Controls.Add(childForm);
+            this.Tag = childForm;
+            //childForm.BringToFront();
+            childForm.Show();
+            /*childForm.Size = new Size(*//*Convert.ToInt32(childForm.Width * 1.155), Convert.ToInt32(childForm.Height * 1.096 - 5)*//*1400, 782);*/
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            /*childForm.Location = new Point(xpos, ypos);*/
         }
     }
 }
