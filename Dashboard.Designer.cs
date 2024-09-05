6c4e548b348e3b191635190a70c09661aef06a2b
rona1900 <hellouniverse190@gmail.com>
2024-09-06 01:26:37 +0800

    dashboards first draft haha

--- Dashboard.Designer.cs
+++ Dashboard.Designer.cs
@@ -28,94 +28,128 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dashboard));
             this.panel3 = new System.Windows.Forms.Panel();
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
-            this.panel4 = new System.Windows.Forms.Panel();
-            this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
-            this.panel3.SuspendLayout();
+            this.HomeBtn = new System.Windows.Forms.PictureBox();
+            this.OrderBtn = new System.Windows.Forms.PictureBox();
+            this.InventoryBtn = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.HomeBtn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OrderBtn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.InventoryBtn)).BeginInit();
             this.SuspendLayout();
-            // 
-            // panel1
-            // 
-            this.panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.panel1.Controls.Add(this.panel4);
-            this.panel1.Controls.Add(this.flowLayoutPanel1);
-            this.panel1.Location = new System.Drawing.Point(136, 12);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1200, 986);
-            this.panel1.TabIndex = 0;
-            // 
-            // panel2
-            // 
-            this.panel2.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.panel2.Controls.Add(this.flowLayoutPanel2);
-            this.panel2.Location = new System.Drawing.Point(1354, 12);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(548, 986);
-            this.panel2.TabIndex = 1;
             // 
             // panel3
             // 
-            this.panel3.BackColor = System.Drawing.Color.Gray;
-            this.panel3.Controls.Add(this.panel2);
-            this.panel3.Controls.Add(this.panel1);
+            this.panel3.BackColor = System.Drawing.Color.PeachPuff;
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel3.Location = new System.Drawing.Point(132, 48);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(1914, 1014);
             this.panel3.TabIndex = 0;
             // 
-            // flowLayoutPanel1
+            // HomeBtn
             // 
-            this.flowLayoutPanel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 105);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(1194, 878);
-            this.flowLayoutPanel1.TabIndex = 0;
+            this.HomeBtn.Image = ((System.Drawing.Image)(resources.GetObject("HomeBtn.Image")));
+            this.HomeBtn.Location = new System.Drawing.Point(-1, 167);
+            this.HomeBtn.Name = "HomeBtn";
+            this.HomeBtn.Size = new System.Drawing.Size(134, 119);
+            this.HomeBtn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.HomeBtn.TabIndex = 1;
+            this.HomeBtn.TabStop = false;
+            this.HomeBtn.Click += new System.EventHandler(this.HomeBtn_Click);
             // 
-            // flowLayoutPanel2
+            // OrderBtn
             // 
-            this.flowLayoutPanel2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.flowLayoutPanel2.Location = new System.Drawing.Point(3, 105);
-            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
-            this.flowLayoutPanel2.Size = new System.Drawing.Size(542, 878);
-            this.flowLayoutPanel2.TabIndex = 0;
+            this.OrderBtn.Image = ((System.Drawing.Image)(resources.GetObject("OrderBtn.Image")));
+            this.OrderBtn.Location = new System.Drawing.Point(-1, 367);
+            this.OrderBtn.Name = "OrderBtn";
+            this.OrderBtn.Size = new System.Drawing.Size(134, 119);
+            this.OrderBtn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.OrderBtn.TabIndex = 2;
+            this.OrderBtn.TabStop = false;
+            this.OrderBtn.Click += new System.EventHandler(this.OrderBtn_Click);
             // 
-            // panel4
+            // InventoryBtn
             // 
-            this.panel4.Location = new System.Drawing.Point(3, 41);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(1194, 57);
-            this.panel4.TabIndex = 1;
+            this.InventoryBtn.Image = ((System.Drawing.Image)(resources.GetObject("InventoryBtn.Image")));
+            this.InventoryBtn.Location = new System.Drawing.Point(-1, 563);
+            this.InventoryBtn.Name = "InventoryBtn";
+            this.InventoryBtn.Size = new System.Drawing.Size(134, 119);
+            this.InventoryBtn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.InventoryBtn.TabIndex = 0;
+            this.InventoryBtn.TabStop = false;
+            this.InventoryBtn.Click += new System.EventHandler(this.InventoryBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Perpetua", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(25, 289);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "HOME";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Perpetua", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(25, 489);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 27);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "ORDER";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Perpetua", 11F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(6, 685);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 22);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "INVENTORY";
             // 
             // Dashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
             this.ClientSize = new System.Drawing.Size(1942, 1102);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.InventoryBtn);
+            this.Controls.Add(this.OrderBtn);
+            this.Controls.Add(this.HomeBtn);
             this.Controls.Add(this.panel3);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Dashboard";
             this.Text = "Dashboard";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
-            this.panel1.ResumeLayout(false);
-            this.panel2.ResumeLayout(false);
-            this.panel3.ResumeLayout(false);
+            this.Load += new System.EventHandler(this.Dashboard_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.HomeBtn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OrderBtn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.InventoryBtn)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
-        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.PictureBox HomeBtn;
+        private System.Windows.Forms.PictureBox OrderBtn;
+        private System.Windows.Forms.PictureBox InventoryBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
